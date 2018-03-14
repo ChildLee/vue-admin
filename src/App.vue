e4e324631d18ea9d311c396b7bb5144dda5893c8
ChildLee <471227142@qq.com>
2018-03-14 18:43:20 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="stylus">
+  body, html {
+    height: 100%;
+    width: 100%
+    margin: 0;
+  }
+
+  #app {
+    height: 100%;
+    width: 100%
+  }
+</style>
